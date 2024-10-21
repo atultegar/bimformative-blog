95171879f9cd959973144f88ff2ab92ee763bbaa
Atul Tegar <ATTE@COWI.com>
2024-10-21 16:22:54 +0200

    added vercelignore

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,6 @@ import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
 import Navbar from "./components/Navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const inter = Inter({subsets: ["latin"] });
 
